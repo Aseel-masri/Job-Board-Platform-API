4b6ef9c7714b35416a2f675ae0399dd62e6e18b5
Aseel-masri <aseel.maasri.2002@gmail.com>
2023-05-19 13:09:45 +0300

    employer

--- employer/services.js
+++ employer/services.js
@@ -1,109 +1,110 @@
 import { PrismaClient } from "@prisma/client";
 import express from 'express';
-import { body, param , query} from "express-validator";
+import { body, param, query } from "express-validator";
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
 // Database functions
-const createEmployer = async (name) => {
-  return await prisma.employer.create({
-    data: {
-      name: name,
-    },
-  });
+const createEmployer = async (name, email) => {
+    return await prisma.employer.create({
+        data: {
+            name: name,
+            email: email
+        },
+    });
 };
 
 const getEmployer = async (req, res, next) => {
-  try {
-    const employers = await prisma.employer.findMany();
-    res.send({
-      success: true,
-      employers: employers,
-    });
-  } catch (error) {
-    res.send({
-      success: false,
-      error: error.message,
-    });
-  }
+    try {
+        const employers = await prisma.employer.findMany();
+        res.send({
+            success: true,
+            employers: employers,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
 };
 
 const deleteEmployer = async (id) => {
     return await prisma.employer.delete({
-      where: {
-        id: id,
-      },
+        where: {
+            id: id,
+        },
     });
-  };
-  const updateEmployer = async (id, { id: _, name }) => {
-    if (!name) {
-      throw new Error("Name is required for update."+name+"+");
-    }
-  
+};
+const updateEmployer = async (id, name, email) => {
+
+
     return await prisma.employer.update({
-      where: {
-        id: parseInt(id),
-      }, data: {
-        name: name
-      }
-      
+        where: {
+            id: parseInt(id),
+        }, data: {
+            name: name,
+            email: email
+        }
+
     });
-  };
-  
+};
+
 
 // Routes
 app.get("", getEmployer);
 app.post("", async (req, res, next) => {
-  try {
-    
-    const { name } = req.body;
-    const employer = await createEmployer(name);
-    res.send({
-      success: true,
-      employer: employer,
-    });
-  } catch (error) {
-    res.send({
-      success: false,
-      error: error.message,
-    });
-  }
+    try {
+
+        const { name } = req.body;
+        const { email } = req.body;
+        const employer = await createEmployer(name, email);
+        res.send({
+            success: true,
+            employer: employer,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
 app.delete("/:id", async (req, res, next) => {
     try {
-      const id = req.params.id;
-      await deleteEmployer(+id);
-      res.send({
-        success: true,
-        message: "Employer deleted successfully",
-      });
+        const id = req.params.id;
+        await deleteEmployer(+id);
+        res.send({
+            success: true,
+            message: "Employer deleted successfully",
+        });
     } catch (error) {
-      res.send({
-        success: false,
-        error: error.message,
-      });
+        res.send({
+            success: false,
+            error: error.message,
+        });
     }
-  });
+});
 
-  app.put("/:id", async (req, res, next) => {
+app.put("/:id", async (req, res, next) => {
     try {
-      const { id } = req.params;
-      const { name } = req.body;
-      const employer = await updateEmployer(id, req.body);
-      res.send({
-        success: true,
-        employer: employer,
-      });
+        const id = req.params.id;
+        const { name, email } = req.body;
+        const employer = await updateEmployer(id, name, email);
+        res.send({
+            success: true,
+            employer: employer,
+        });
     } catch (error) {
-      res.send({
-        success: false,
-        error: error.message,
-      });
+        res.send({
+            success: false,
+            error: error.message,
+        });
     }
-  });
+});
 
-  export default app
-  
-  
+export default app
+
+
