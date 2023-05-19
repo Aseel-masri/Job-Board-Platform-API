7702b1521e7cd21f6f7f8c47ceda756b765a59fe
Aseel-masri <aseel.maasri.2002@gmail.com>
2023-05-20 00:01:23 +0300

    User Login

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import job from "./jobListingTable/jobListing.js";
 import emp from "./employer/services.js";
 import seek from "./seek/seek.js";
-
+import log from "./User/user.js"
 import express from 'express';
 
 const app = express();
@@ -9,6 +9,7 @@ console.log("dn");
 app.use("/employer",emp);
 app.use("/joblistings",job);
 app.use("/seekers",seek);
+app.use("/login",log);
 
 // Listen to port
 app.listen(8081, () =>
