f1d7b82e32f4475d2ab01b2a7563c1ec5fb17903
lindaabukhalil <lindaabukhalil2001@gmail.com>
2023-05-19 18:06:38 +0300

    12

--- test.js
+++ test.js
@@ -8,11 +8,8 @@ const app = express();
 console.log("dn");
 app.use("/employer",emp);
 app.use("/joblistings",job);
-<<<<<<< HEAD
 app.use("/seekers",seek);
 
-=======
->>>>>>> main
 // Listen to port
 app.listen(8081, () =>
   console.log("🚀 Server ready at: http://localhost:8081")
